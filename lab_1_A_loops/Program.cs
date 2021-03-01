f2a000adfb44d005b240a24476e7a65dfdae5c7e
MrSoulfinder <ilyachernookiy_79@mail.ru>
2021-03-01 19:30:42 +0300

    lab

---
+++ lab_1_A_loops/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lab_1_AND_loop
+{
+    class Program
+    {
+        static long Fact(long value)
+        {
+            return (value == 0) ? 1 : value * Fact(value - 1);
+        }
+            static void Main(string[] args)
+        {
+            Console.WriteLine("Input k: ");
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input x: ");
+            double x = double.Parse (Console.ReadLine());
+            double s = 0;
+
+            for (int n = 0; n <= k; n++)
+                s += Math.Pow(-1, n) * Math.Pow(x, 2*n + 1) / Fact(2*n + 1);
+
+            Console.WriteLine("Result of given sum: {0}", s.ToString());
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ lab_ex_1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число, из которого вычислять корень: ");
+            double y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите корень: ");
+            double z = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ответ методом Ньютона: ");
+            Console.WriteLine(SqrtN(z, y));
+
+            Console.WriteLine("Ответ методом Math.Pow: ");
+            double v = Math.Pow(y, 1/z);
+            Console.WriteLine(v);
+            Console.ReadKey();
+        }
+
+        static double Pow(double a, int pow)
+        {
+            double result = 1;
+            for (int i = 0; i < pow; i++) result *= a;
+            return result;
+        }
+
+        static double SqrtN(double n, double A, double eps = 0.0001)
+        {
+            var x0 = A / n;
+            var x1 = (1 / n) * ((n - 1) * x0 + A / Pow(x0, (int)n - 1));
+
+            while (Math.Abs(x1 - x0) > eps)
+            {
+                x0 = x1;
+                x1 = (1 / n) * ((n - 1) * x0 + A / Pow(x0, (int)n - 1));
+            }
+
+            return x1;
+        }
+    }
+}
