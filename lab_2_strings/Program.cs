267a9732bd44ca8da9d3266cdf523f16b73d8793
MrSoulfinder <ilyachernookiy_79@mail.ru>
2021-03-08 20:48:37 +0300

    lab

---
+++ lab_2_strings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace lab_2_strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+            StreamReader infile = new StreamReader(@"C:\git\lab_2_strings\Inlet.txt");
+            StreamWriter outfile = new StreamWriter(@"C:\git\lab_2_strings\Outlet.txt");
+
+            int minInd = 0;
+            string intext = infile.ReadToEnd();
+            string[] dd = intext.Split(' ');
+                int min = dd[0].Length;
+
+                for (int i = 0; i < dd.Length; i++)
+                    if (min > dd[i].Length) 
+                { 
+                    min = dd[i].Length; minInd = i; 
+                }
+
+            outfile.Write("самое кроткое слово: {0} ", dd[minInd]);
+            outfile.Write("индекс самого кроткого слова: {0} ", minInd);
+            
+            Console.ReadLine();
+            }
+    }
+}
---
+++ lab_2_vectors/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace lab_2_vectors
+{
+    class Program
+    {
+            static void Main()
+            {
+                Console.WriteLine("Введите делитель делитель: ");
+                int c = int.Parse(Console.ReadLine());
+                File.WriteAllText(@"C:\git\lab_2_vectors\Outlet.txt", String.Join(Environment.NewLine, File.ReadAllText(@"C:\git\lab_2_vectors\Inlet.txt")
+                    .Split(' ').ToArray().Where((v, i) => c % (i + 1) == 0).ToArray()));
+            }
+        
+    }
+}
