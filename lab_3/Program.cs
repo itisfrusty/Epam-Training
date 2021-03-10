f13cc1098f4e4df31afed438cfd3520fafd0e3a8
MrSoulfinder <ilyachernookiy_79@mail.ru>
2021-03-10 18:21:50 +0300

    labs

---
+++ lab_3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace lab_3
+{
+    abstract class Figure
+    {
+        public abstract string Area();
+        public abstract string Perimeter();
+        public abstract string ShapeName();
+
+        public void ShowInfo()
+        {
+            Console.WriteLine(
+            $"Название фигуры: {ShapeName()}\n" +
+            $"Площадь: {Area()}\n" +
+            $"Периметр: {Perimeter()}"
+            );
+            Console.WriteLine();
+        }
+    }
+
+    class Rectangle : Figure
+    {
+        double sideA;
+        double sideB;
+        public Rectangle(double rectSideA, double rectSideB)
+        {
+            SideA = rectSideA;
+            sideB = rectSideB;
+        }
+        public double SideA
+        {
+            get { return sideA; }
+            set { sideA = value < 0 ? -value : value; }
+        }
+        public override string Area()
+        {
+            return (sideA * sideB).ToString(); // формула нахождения площади прямоугольника 
+                                                                                        
+        }
+
+        public override string Perimeter()
+        {
+            return ((sideA + sideB)*2).ToString(); // формула нахождения периметра прямоугольника
+        }
+        public override string ShapeName()
+        {
+            return "Прямогугольник";
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Figure figure1 = new Rectangle(2,4);
+            figure1.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
