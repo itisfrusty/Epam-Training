b45a96a98438073b9772ad48fe270a834a5434f0
MrSoulfinder <ilyachernookiy_79@mail.ru>
2021-03-22 19:16:34 +0300

    labs

--- lab_5/Program.cs
+++ lab_5/Program.cs
@@ -1,76 +1,222 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace lab_5
+namespace ConsoleApplication1
 {
-    class Program
+    class Matrix
     {
-        class Matrix
+        // Скрытые поля
+        private int n;
+        private int[,] mass;
+
+        // Создаем конструкторы матрицы
+        public Matrix() { }
+        public int N
         {
-            public float[,] array { get; }
-            public int cols { get; }
-            public int rows { get; }
-            public Matrix(int cols, int rows)
+            get { return n; }
+            set { if (value > 0) n = value; }
+        }
+
+        // Задаем аксессоры для работы с полями вне класса Matrix
+        public Matrix(int n)
+        {
+            this.n = n;
+            mass = new int[this.n, this.n];
+        }
+        public int this[int i, int j]
+        {
+            get
             {
-                this.cols = cols;
-                this.rows = rows;
-                array = new float[cols, rows];
+                return mass[i, j];
             }
-            public void Fill()
+            set
             {
-                Random r = new Random();
-                for (int i = 0; i < cols; i++)
+                mass[i, j] = value;
+            }
+        }
+
+        // Ввод матрицы с клавиатуры
+        public void WriteMat()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
                 {
-                    for (int j = 0; j < rows; j++)
+                    Console.WriteLine("Введите элемент матрицы {0}:{1}", i + 1, j + 1);
+                    mass[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+
+        // Вывод матрицы с клавиатуры
+        public void ReadMat()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(mass[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+        // Проверка матрицы А на единичность
+        public void oneMat(Matrix a)
+        {
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (a[i, j] == 1 && i == j)
                     {
-                        array[i, j] = r.Next(100);
+                        count++;
                     }
                 }
+
             }
-            public void Show()
+            if (count == a.N)
             {
-                for (int i = 0; i < cols; i++)
-                {
-                    for (int j = 0; j < rows; j++)
-                    {
-                        Console.Write(array[i, j] + " ");
-                    }
-                    Console.WriteLine();
-                }
+                Console.WriteLine("Единичная");
             }
-            public void Addition(float[,] array2)
+            else Console.WriteLine("Не единичная");
+        }
+
+
+        // Умножение матрицы А на число
+        public static Matrix umnch(Matrix a, int ch)
+        {
+            Matrix resMass = new Matrix(a.N);
+            for (int i = 0; i < a.N; i++)
             {
-                if (array.Length == array2.Length)
+                for (int j = 0; j < a.N; j++)
                 {
-                    for (int i = 0; i < cols; i++)
-                    {
-                        for (int j = 0; j < rows; j++)
-                        {
-                            array[i, j] += array2[i, j];
-                        }
-                    }
-                }
-                else
-                {
-                    throw new Exception("Ваша ошибка.");
+                    resMass[i, j] = a[i, j] * ch;
                 }
             }
-            public void Multiplication(float[,] array2)
+            return resMass;
+        }
+
+        // Умножение матрицы А на матрицу Б
+        public static Matrix umn(Matrix a, Matrix b)
+        {
+            Matrix resMass = new Matrix(a.N);
+            for (int i = 0; i < a.N; i++)
+                for (int j = 0; j < b.N; j++)
+                    for (int k = 0; k < b.N; k++)
+                        resMass[i, j] += a[i, k] * b[k, j];
+
+            return resMass;
+        }
+
+        // Перегрузка оператора умножения
+        public static Matrix operator *(Matrix a, Matrix b)
+        {
+            return Matrix.umn(a, b);
+        }
+
+        public static Matrix operator *(Matrix a, int b)
+        {
+            return Matrix.umnch(a, b);
+        }
+
+
+        // Метод вычитания матрицы Б из матрицы А
+        public static Matrix razn(Matrix a, Matrix b)
+        {
+            Matrix resMass = new Matrix(a.N);
+            for (int i = 0; i < a.N; i++)
             {
-                if (array.Length == array2.Length)
+                for (int j = 0; j < b.N; j++)
                 {
-                    for (int i = 0; i < cols; i++)
-                    {
-                        for (int j = 0; j < rows; j++)
-                        {
-                            array[i, j] *= array2[i, j];
-                        }
-                    }
-                }
-                else
-                {
-                    throw new Exception("Ваша ошибка.");
+                    resMass[i, j] = a[i, j] - b[i, j];
                 }
             }
+            return resMass;
+        }
+
+        // Перегрузка оператора вычитания
+        public static Matrix operator -(Matrix a, Matrix b)
+        {
+            return Matrix.razn(a, b);
+        }
+        public static Matrix Sum(Matrix a, Matrix b)
+        {
+            Matrix resMass = new Matrix(a.N);
+            for (int i = 0; i < a.N; i++)
+            {
+                for (int j = 0; j < b.N; j++)
+                {
+                    resMass[i, j] = a[i, j] + b[i, j];
+                }
+            }
+            return resMass;
+        }
+        // Перегрузка сложения
+        public static Matrix operator +(Matrix a, Matrix b)
+        {
+            return Matrix.Sum(a, b);
+        }
+        // Деструктор Matrix
+        ~Matrix()
+        {
+            Console.WriteLine("Очистка");
+        }
+
+    }
+    class MainProgram
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность матрицы: ");
+            int nn = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите целую константу: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            // Инициализация матрицы
+            Matrix mass1 = new Matrix(nn);
+            Matrix mass2 = new Matrix(nn);
+            Matrix mass3 = new Matrix(nn);
+            Matrix mass4 = new Matrix(nn);
+            Matrix mass5 = new Matrix(nn);
+            Matrix mass6 = new Matrix(nn);
+            Matrix mass7 = new Matrix(nn);
+            Matrix mass8 = new Matrix(nn);
+            Console.WriteLine("ввод Матрица А: ");
+            mass1.WriteMat();
+            Console.WriteLine("Ввод Матрица B: ");
+            mass2.WriteMat();
+
+            Console.WriteLine("Матрица А: ");
+            mass1.ReadMat();
+            Console.WriteLine();
+            Console.WriteLine("Матрица В: ");
+            Console.WriteLine();
+            mass2.ReadMat();
+
+            Console.WriteLine("Сложение матриц А и Б: ");
+            mass4 = (mass1 + mass2);
+            mass4.ReadMat();
+
+            Console.WriteLine("Вычитание матриц А и Б: ");
+            mass6 = (mass1 - mass2);
+            mass6.ReadMat();
+
+            Console.WriteLine("Умножение матриц А и Б: ");
+            mass8 = (mass1 * mass2);
+            mass8.ReadMat();
+
+            Console.WriteLine("Умножение матрицы А на число a: ");
+            mass5 = (mass1 * a);
+            mass5.ReadMat();
+
+            Console.ReadKey();
         }
     }
 }
